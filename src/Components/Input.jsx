b3ce9fcbe15aa140c2662e729e2036a19299794b
Teekay1702 <tokoloho57@gmail.com>
2025-02-11 19:55:12 +0200

    added Input field

---
+++ src/Components/Input.jsx
@@ -0,0 +1,26 @@
+input.jsx
+import React from "react";
+
+const Input = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitted");
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter your search term"
+                    autoComplete="off"
+                    className="input"
+                />
+                <button type="submit" className="btn">
+                    Search
+                </button>
+            </form>
+        </div>
+    )
+}
+export default Input;
